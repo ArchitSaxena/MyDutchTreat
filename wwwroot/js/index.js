bd63ff534cae85b072bf2fe4a10511a5e540377b
Archit Saxena <Archit.Saxena@mrcooper.com>
2020-02-15 21:49:13 +0530

    initial commit

---
+++ wwwroot/js/index.js
@@ -0,0 +1,27 @@
+﻿var x = 10;
+var s = "";
+
+//alert("Welcome to pluralsight");
+console.log("Welcome to Plural Sight");
+
+var theForm = $("#theForm");
+theForm.hide();
+
+var buyButton = $("#buyButton");
+buyButton.on("click", function () {
+    alert("Buying Item");
+    console.log("Item bought");
+});
+
+var productInfo = $(".product-props li");
+productInfo.on("click", function () {
+    console.log("You clicked on-" + $(this).text());
+})
+
+
+var toggleForm = $("#ToggleForm");
+var popUpForm = $(".PopUpForm");
+
+toggleForm.on("click", function () {
+    popUpForm.slideToggle(500);
+});
